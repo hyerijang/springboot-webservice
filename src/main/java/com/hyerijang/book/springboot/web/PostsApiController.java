d613b090439d5166339fd2d9b84b21fa4f04c947
Hyeri Jang <chjang5621@naver.com>
2021-08-04 19:00:36 +0900

    HTTP메서드 및 REST 관련 주석 추가

--- src/main/java/com/hyerijang/book/springboot/web/PostsApiController.java
+++ src/main/java/com/hyerijang/book/springboot/web/PostsApiController.java
@@ -12,12 +12,17 @@ import org.springframework.web.bind.annotation.*;
 public class PostsApiController {
 
     private final PostService postService;
-
+    //*HTTP메서드*
+    //GET은 서버의 리소스에서 데이터를 요청시
+    //POST는 서버의 리소스를 새로 생성(+ 업데이트)할 때 사용
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postService.save(requestDto);
     }
 
+    //*REST방식*
+    //실제로는 요청을 보낼 때 GET, POST 외에도 PUT, PATCH, DELETE 등 더 자세한 동작을 정의할 수 있습니다.
+    //보통 PUT은 전체 수정(대체), PATCH는 부분 수정, DELETE는 제거 요청 시 사용합니다.
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postService.update(id,requestDto);
